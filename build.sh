547da90ef1af1229f270040abce0c270376986c0
Alex Badea <vamposdecampos@gmail.com>
2011-01-26 22:56:14 +0200

    Add syncgen testbench

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ mkdir -p work
 for f in src/*.vhdl ; do
 	ghdl -a --workdir=work  $f
 done
-for module in tb_hcount tb_vcount ; do
+for module in tb_hcount tb_vcount tb_syncgen ; do
 	ghdl -e --workdir=work $module
 done
 
