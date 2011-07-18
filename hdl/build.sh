35f97ea0ae04b408e50c2b903177032d24bf5598
Alex Badea <vamposdecampos@gmail.com>
2011-07-18 20:48:50 +0300

    hdl: move test-benches under a sim/ directory

--- hdl/build.sh
+++ hdl/build.sh
@@ -5,9 +5,13 @@ cd $(dirname $0)
 mkdir -p work
 
 for f in src/*.vhdl ; do
-	ghdl -a --workdir=work  $f
+	ghdl -a --workdir=work $f
 done
-for module in tb_hcount tb_vcount tb_syncgen tb_pixels tb_arbiter ; do
+for f in src/sim/*.vhdl ; do
+	ghdl -a --workdir=work $f
+done
+for f in src/sim/tb_* ; do
+	module=$(basename $f | sed -e 's/\.vhdl$//')
 	ghdl -e --workdir=work $module
 done
 
