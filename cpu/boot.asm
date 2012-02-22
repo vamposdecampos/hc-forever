c4883533eff84d548b27102d368b8f055083cb00
Alex Badea <vamposdecampos@gmail.com>
2012-02-22 09:26:04 +0200

    cpu bootloader: add keyboard input
    Present a list of selections, wait for a keypress, and pretend to act
    on it.

--- cpu/boot.asm
+++ cpu/boot.asm
@@ -1,4 +1,4 @@
-org 0xa000
+org 0x6000
 
 screen		equ	0x4000
 pixels_len	equ	6144
@@ -7,12 +7,12 @@ attributes_len	equ	768
 
 ; variables
 cursor		equ	0x5b00
+key		equ	0x5b02
 stack		equ	0x5c00
 
 main proc
 
 	di
-	ld	hl, stack
 	ld	sp, stack
 
 ; clear screen
@@ -49,10 +49,42 @@ _rgbfill:
 	ld	de, banner
 	call	puts
 
-; and, cut.
+; read key
+menu:
+	ld	bc, 0xf7fe	; keys 1..5
+	in	a, (c)
+	cpl
+	and	0x1f
+	ld	(key), a
+	or	a
+	jr	z, menu
+_wait_release:
+	in	a, (c)
+	and	0x1f
+	cp	0x1f
+	jr	nz, _wait_release
+
+	ld	a, (key)	; can be 1, 2, 4, 8, 16
+	cp	1
+	jr	z, boot_maskrom
+	cp	2
+	jr	z, boot_spiflash
+	jr	menu
+
+boot_maskrom:
+	ld	de, str_maskrom
+	call	puts
+	; TODO
+	ld	a, 3
+	out	(0xfe), a
+	halt
+
+boot_spiflash:
+	ld	de, str_spiflash
+	call	puts
+	; TODO
 	ld	a, 4
 	out	(0xfe), a
-	di
 	halt
 
 
@@ -64,7 +96,12 @@ banner:
 	db 10, 10
 	db "Bootloader "
 	incbin "version.gen.txt"
+	db 10
+	db "Boot:  1=mask ROM  2=SPI flash", 10
 	db 0
+
+str_maskrom:	db "mask ROM... ", 0
+str_spiflash:	db "SPI flash... ", 0
 
 endp
 end main
