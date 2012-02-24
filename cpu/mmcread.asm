225b22f90b7356501fa63c18f9a76a2546114123
Alex Badea <vamposdecampos@gmail.com>
2012-02-24 04:08:57 +0200

    cpu mmcread: working mmc-to-spiflash copy
    Read 0x50000 bytes from the start of the SD card, and write
    it into the SPI flash.  This upgrades the FPGA bitstream.
    
    Tested with a 1GB kingston SD card.

--- cpu/mmcread.asm
+++ cpu/mmcread.asm
@@ -11,17 +11,26 @@ MMC_READ_SINGLE_BLOCK		equ	0x51
 ; Spectrum ROM routines
 po_msg		equ	0x0c0a
 
+WREN		equ	0x06
+WRDI		equ	0x04
+RDID		equ	0x9f
+RDSR		equ	0x05
+PAGE_PROGRAM	equ	0x02
+SECTOR_ERASE	equ	0xd8
 
-assert_cs macro
-	di
-	ld	a, 1				; VS0
+spi_assert_cs macro
+	ld	a, 3				; on-board SPI flash chip (m25p32)
+	out	(PORT_SPI_SELECT), a
+endm
+
+mmc_assert_cs macro
+	ld	a, 1				; VS0 (SD card)
 	out	(PORT_SPI_SELECT), a
 endm
 
 release_cs macro
 	xor	a
 	out	(PORT_SPI_SELECT), a
-	ei
 endm
 
 
@@ -49,7 +58,7 @@ _init_loop:
 	rst	0x10
 
 ; enter SPI mode
-	assert_cs
+	mmc_assert_cs
 	ld	a, MMC_RESET
 	out	(PORT_SPI_DATA), a
 	xor	a
@@ -81,7 +90,7 @@ set_operstate:
 	out	(PORT_SPI_DATA), a
 	nop
 
-	assert_cs
+	mmc_assert_cs
 	ld	a, MMC_SEND_OPERATING_STATE
 	ld	hl, 0
 	ld	de, 0
@@ -95,7 +104,7 @@ set_operstate:
 	cp	0
 	jr	z, oper_done
 	djnz	set_operstate
-	jr	fail
+	jp	fail
 
 oper_done:
 	ld	a, 13
@@ -103,47 +112,80 @@ oper_done:
 	ld	a, 'B'
 	rst	0x10
 
-	assert_cs
+	mmc_assert_cs
 	ld	a, MMC_SET_BLOCKLEN
 	ld	hl, 0
-	ld	de, 0x0200
+	ld	de, 0x0100
 	call	mmc_command
 	ld	b, 8
 	call	mmc_wait_response
 	release_cs
 
-;read 
+; erase sectors
+	ld	de, 0
+	ld	(block_off), de
+do_erase:
+	call	spi_wren
+	call	spi_erase
+	call	spi_wait
+
+	ld	de, (block_off)
+	inc	d			; 64 KB (512 kbits)
+	ld	(block_off), de
+	ld	a, d
+	cp	5
+	jr	nz, do_erase
+
+	ld	a, 13
+	rst	0x10
+
+	ld	de, 0
+	ld	(block_off), de
+
+;read & flash
+do_flash:
+	ld	hl, (block_off)
+	ld	a, h
+	call	po_hex
+	ld	a, l
+	call	po_hex
+
 	ld	a, 13
 	rst	0x10
 	ld	a, 'R'
 	rst	0x10
 
-	assert_cs
+	mmc_assert_cs
 	ld	a, MMC_READ_SINGLE_BLOCK
-	ld	hl, 0
-	ld	de, 0
+	ld	de, (block_off)
+	ld	h, 0		; HL:DE == byte offset
+	ld	l, d
+	ld	d, e
+	ld	e, 0
 	call	mmc_command
 	ld	b, 1
 	call	mmc_wait_response
 	ld	a, (readbuf)
 	cp	0
-	jr	nz, _read_fail
+	jr	nz, read_fail
 	ld	a, 'r'
 	rst	0x10
 	ld	b, 1
 	call	mmc_wait_response
 	ld	a, (readbuf)
 	cp	0xfe
-	jr	nz, _read_fail
-	ld	bc, 0x0200
+	jr	nz, read_fail
+	ld	bc, 0x0100
 	call	mmc_read_data
 	release_cs
+
+	jr	skip_print	; XXX
 
 	ld	a, 13
 	rst	0x10
 
 ; print
-	ld	bc, 0x0200
+	ld	bc, 0x0100
 	ld	hl, readbuf
 _print_loop:
 	ld	a, (hl)
@@ -154,10 +196,25 @@ _print_loop:
 	or	c
 	jr	nz, _print_loop
 
+skip_print:
+	call	spi_wren
+	call	spi_wait
+	call	spi_program
+	call	spi_wait
+	ld	a, 13
+	rst	0x10
+
+	ld	de, (block_off)
+	inc	de
+	ld	(block_off), de
+	ld	a, d
+	cp	5
+	jr	nz, do_flash
+; done
 	ei
 	ret
 
-_read_fail:
+read_fail:
 	release_cs
 
 fail:
@@ -182,23 +239,23 @@ _dwell_loop:
 	pop	bc
 	ret
 
-; A=command; D, E, H, L=args
+; A=command; H, L, D, E=args
 mmc_command:
 	out	(PORT_SPI_DATA), a
-	nop
-	ld	a, d
-	out	(PORT_SPI_DATA), a
-	nop
-	nop
-	ld	a, e
-	out	(PORT_SPI_DATA), a
-	nop
 	nop
 	ld	a, h
 	out	(PORT_SPI_DATA), a
 	nop
 	nop
 	ld	a, l
+	out	(PORT_SPI_DATA), a
+	nop
+	nop
+	ld	a, d
+	out	(PORT_SPI_DATA), a
+	nop
+	nop
+	ld	a, e
 	out	(PORT_SPI_DATA), a
 	nop
 	nop
@@ -237,7 +294,6 @@ _rdloop:
 	djnz	_rdloop
 	ret
 
-
 mmc_read_data:
 	ld	hl, readbuf
 _rdloop:
@@ -251,6 +307,83 @@ _rdloop:
 	or	c
 	jr	nz, _rdloop
 	ret
+
+;;;;;;
+; SPI stuff
+
+spi_wren:
+	ld	a, 'w'
+	rst	0x10
+	spi_assert_cs
+	ld	a, WREN
+	out	(PORT_SPI_DATA), a
+	release_cs
+	ret
+
+
+spi_erase:
+	ld	a, 'E'
+	rst	0x10
+	spi_assert_cs
+	ld	a, SECTOR_ERASE
+	out	(PORT_SPI_DATA), a
+	ld	de, (block_off)
+	ld	a, d
+	out	(PORT_SPI_DATA), a
+	ld	a, e
+	out	(PORT_SPI_DATA), a
+	xor	a
+	out	(PORT_SPI_DATA), a
+	release_cs
+	ret
+
+spi_program:
+	ld	a, 'P'
+	rst	0x10
+	spi_assert_cs
+	ld	a, PAGE_PROGRAM
+	out	(PORT_SPI_DATA), a
+	ld	de, (block_off)
+	ld	a, d
+	out	(PORT_SPI_DATA), a
+	ld	a, e
+	out	(PORT_SPI_DATA), a
+	xor	a
+	out	(PORT_SPI_DATA), a
+
+	ld	hl, readbuf
+	ld	b, 0
+_loop:
+	ld	a, (hl)
+	out	(PORT_SPI_DATA), a
+	inc	hl
+	inc	de
+	djnz	_loop
+
+	release_cs
+	ret
+
+spi_wait:
+	spi_assert_cs
+	ld	a, RDSR
+	out	(PORT_SPI_DATA), a
+_wait_loop:
+	ld	a, '.'
+	rst	0x10
+	out	(PORT_SPI_DATA), a
+	in	a, (PORT_SPI_DATA)
+	push	af
+	call	po_hex
+	pop	af
+	and	1
+	jr	nz, _wait_loop
+	ld	a, '!'
+	rst	0x10
+	release_cs
+	ret
+
+
+;;;;;;
 
 po_hex:
 	push	af
@@ -283,6 +416,8 @@ banner:		db	"mmcread", 13 + 128
 str_cid:	db	"cid:", ' ' + 128
 str_ptr:	db	"data:", ' ' + 128
 hex_chars:	db	"0123456789abcdef"
+
+block_off:	dw	0		; bits 24..8 of the address
 
 readbuf:
 ; 512 bytes
